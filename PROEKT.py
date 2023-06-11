f8040d352f8f3849b707bc37c8b9a8684121ea9c
Pavel3795 <132829906+Pavel3795@users.noreply.github.com>
2023-06-11 15:53:23 +0300

    Update PROEKT.py

--- PROEKT.py
+++ PROEKT.py
@@ -1,21 +1,24 @@
-import os
-import discord
-from discord.ext import commands
-from random import choice
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.command()
-async def mem(ctx):
-    image_name = choice(os.listdir('images'))
-    with open(f'images/{image_name}', 'rb') as f:
-        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
-        picture = discord.File(f)
-   # Можем передавать файл как параметр!
-    await ctx.send(file=picture)
+import random
 
 
-bot.run('MTEwOTg0NjkzMDY1NTI4OTM0NA.G_vz80.2njblC26JKFmOVdIBNjv6NeW2yzYn-6Cu9CIrI')
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
