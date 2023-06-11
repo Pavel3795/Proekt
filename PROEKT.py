01e8a39f63833dcca66628a4a2baa319bc1ab84f
Pavel3795 <132829906+Pavel3795@users.noreply.github.com>
2023-06-11 09:24:07 +0300

    Update PROEKT.py

--- PROEKT.py
+++ PROEKT.py
@@ -1,13 +1,21 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "РОФЛ": "Шутка",
-            "ЩИЩ": "Одобрение или восторг",
-            "КРИПОВЫЙ": "Страшный, пугающий",
-            "АГРИТЬСЯ": "Злиться",
-            }
-word = input("Введите непонятное слово (большими буквами!): ").upper()
-if word in meme_dict.keys():
-    print (meme_dict[word])
-else:
-    print("Такого слва нету :(")
+import os
+import discord
+from discord.ext import commands
+from random import choice
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def mem(ctx):
+    image_name = choice(os.listdir('images'))
+    with open(f'images/{image_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+
+bot.run('MTEwOTg0NjkzMDY1NTI4OTM0NA.G_vz80.2njblC26JKFmOVdIBNjv6NeW2yzYn-6Cu9CIrI')
